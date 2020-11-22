478a1933e260e8ddf194ff1947269df0a4d86a1b
Manas Kaul <kaulmanascocid@gmail.com>
2020-11-23 00:02:17 +0530

    added login selection page

---
+++ lib/screens/authentication/login_selection.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class LoginSelection extends StatelessWidget {
+
+  final List<String> languages = ["English (United States)", "Hindi"];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  items: languages.map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  value: languages[0],
+                  onChanged: (_) {},
+                ),
+              ),
+            ),
+
+            SizedBox(height: 220.0),
+
+            Image.asset(
+              "assets/images/instagram_typography.png",
+              height: 100.0,
+              width: 200.0,
+            ),
+
+            SizedBox(height: 40.0),
+
+            Container(
+              padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+              height: 50.0,
+              width: double.infinity,
+              child: FlatButton(
+                child: Text(
+                  "Create New Account",
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                ),
+                onPressed: () {},
+              ),
+            ),
+
+            SizedBox(height: 15.0),
+
+            TextButton(
+              child: Text(
+                "Log In",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/authentication/login_selection.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginSelection();
+  }
+}
